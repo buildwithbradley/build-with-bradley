be208cd0a4f32c9d3df0da04d3d9a9a8ed3de35c
buildwithbradley <bradley@buildwithbradley.dev>
2025-04-13 21:48:23 +0100

    Site Initial Redesign (navbar, logos)
    Changes:
    Restructed file management, edited pages, added navbar
    Added: navbar.html, navbar-loader.js, base.css, navbar.css, logo images
    Deleted: aboutme.htm, contactme.html, journey.html and projects.html

---
+++ js/navbar-loader.js
@@ -0,0 +1,9 @@
+// js/navbar-loader.js
+fetch('elements/navbar.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('navbar').innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Error loading navbar:', error);
+  });
